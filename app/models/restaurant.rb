41ff778027bb16aad23d2f5dff980b4ecb61ca23
pierrebollack <pierre.bollack@edhec.com>
2021-06-09 14:46:50 +0200

    restaurants controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "%{value} is not a valid size"}
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "%{value} is not a valid category"}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+La_Dubs = { name: 'La Dubs', phone_number: "03 88 67 67 56",  address: '4 Rue des Vieux Murs, 59000 Lille' ,  category: 'french'}
+La_Paulo = { name: 'La Paulo' , phone_number: "03 88 67 67 56", address: '126 Rue Esquermoise, 59800 Lille' , category: 'italian'}
+La_Tim ={ name: 'La Tim' , phone_number: "03 88 67 67 56",  address: '14 Rue des Postes, 59000 Lille' ,  category: 'french'}
+La_Gasp = { name: 'La Gasp' , phone_number: "03 88 67 67 56", address: '126 Rue princesse, 59800 Lille' ,  category: 'french'}
+La_Pierrot = { name: 'La Pierrot' , phone_number: "03 88 67 67 56",  address: '3Bis Place Sébastopol 59000 Lille' ,  category: 'italian'}
+[La_Dubs, La_Paulo, La_Tim, La_Gasp, La_Pierrot].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"

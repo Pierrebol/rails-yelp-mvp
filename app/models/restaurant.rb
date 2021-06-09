79ace45b68b074f758fdb62b0114a9c485871a07
pierrebollack <pierre.bollack@edhec.com>
2021-06-09 12:43:14 +0200

    created the 2 models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, presence: true
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "%{value} is not a valid size"}
 end
